3c9546dada2d7d6b678b79e30507ea4f2b6bc339
harsh200603 <harshgupta.ncer@gmail.com>
2024-07-28 23:25:10 +0530

    newfile

---
+++ PrintSubString.java
@@ -0,0 +1,9 @@
+package DSA;
+
+public class PrintSubString {
+    public static void main(String[] args) {
+        String input="12345678";
+        String printsub="";
+        System.out.println(input.substring(2,8);
+    }
+}
